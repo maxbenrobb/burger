1edec3d7ce6d3eee2981f8b52eb847b4223dbc8d
Max Robb <maxbenrobb@wifi_guest-10-38-126-205.uncc.edu>
2018-04-17 13:22:48 -0400

    semi functional code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,45 @@
 // burger.js
 // Create the router for the app, and export the router at the end of your file.
 
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var burgObject = {
+            burger: data
+        };
+        console.log(burgObject);
+        res.render("index", burgObject);
+    });
+});
+
+router.post("/api/food", function(req, res) {
+    burger.create(["name", "devour"], [req.body.name, req.body.devour], function(result) {
+        res.json({ id: result.insertID});
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update(
+        {
+            devour: req.body.sleepy
+        },
+        condition,
+        function(result) {
+            if (result.changeRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
